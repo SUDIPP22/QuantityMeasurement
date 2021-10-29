416c1fa5b649f22ead5d5d05e9005626d214c5d2
Sudip Panja <panja370@gmail.com>
2021-10-29 17:00:30 +0530

    Added : Test cases for checking equality, null, reference, type and value for inch entity

--- src/test/java/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/quantitymeasurement/QuantityMeasurementTest.java
@@ -36,6 +36,41 @@ public class QuantityMeasurementTest {
     void givenValue0FeetAnd1Feet_ShouldReturnNotEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(1.0);
-        Assertions.assertEquals(feet1, feet2);
+        Assertions.assertNotEquals(feet1, feet2);
+    }
+
+    @Test
+    void given0InchAnd0Inch_ShouldReturnEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(0.0);
+        Assertions.assertEquals(inch1, inch2);
+    }
+
+    @Test
+    void given0InchAndNullInch_ShouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = null;
+        Assertions.assertNotEquals(inch1, inch2);
+    }
+
+    @Test
+    void givenReference0InchAnd1Inch_ShouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(1.0);
+        Assertions.assertNotEquals(inch1, inch2);
+    }
+
+    @Test
+    void givenType0InchAnd1Inch_ShouldReturnEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(1.0);
+        Assertions.assertEquals(inch1.getClass(), inch2.getClass());
+    }
+
+    @Test
+    void givenValue0InchAnd1Inch_ShouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(1.0);
+        Assertions.assertNotEquals(inch1, inch2);
     }
 }
